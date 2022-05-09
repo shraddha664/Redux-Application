465ec9b16e9ef2b024cb953b4dcbf15a0510073e
Shraddha Parajuli <shraddhaparajuli834@gmail.com>
2022-05-09 17:46:26 +0545

    redux application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css"
+import {useSelector,useDispatch} from "react-redux";
+import { incNumber } from './actions/index'
+import { decNumber } from './actions/index'
+ 
+const App = () => {
+   const myState=useSelector((state)=>state.changeTheNumber);
+  const dispatch=useDispatch();
+   return (
+    <div className="container">
+      <h1> Increment/Decrement Counter</h1>
+      <h4>
+Using React and Redux
+      </h4>
+      <div className="quantity">
+        <a className="quantity-minus" title="Decrement" onClick={()=>dispatch(decNumber())}><span>-</span></a>
+        <input name="quantity" type="text" className="quantity-input" value={myState} />
+        <a className="quantity-plus" title="Increment" onClick={()=>dispatch(incNumber(5))} ><span>+</span></a>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
